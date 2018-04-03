7b08307c3cddb32fe48f743dd18d2ed527fcfcae
xuxin <793307910@qq.com>
2018-04-03 23:01:56 +0800

    歌单页面歌曲序号修改为01~10

--- src/javascript/component/renderAlbum.js
+++ src/javascript/component/renderAlbum.js
@@ -8,9 +8,10 @@ define(function(){
             setTimeout(function(){
                 var songList = document.querySelector('.songList')
                 self.ret.forEach(function(value,index){
+                    index = (index+1)<10?'0'+(index+1):index+1;
                     var li = document.createElement('li');
                     li.classList.add('song');
-                    li.innerHTML = `<div class="Number">${index+1}</div><a href="./song.html?id=${value.id}">`+
+                    li.innerHTML = `<div class="Number">${index}</div><a href="./song.html?id=${value.id}">`+
                     `<h4 class="songName">${value.title}</h4>`+
                     `<p class="describe"><span class="SQ"></span>${value.author} - ${value.title}</p>`+
                     `<span class="play"></span></a>`;
